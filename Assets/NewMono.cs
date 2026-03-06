9b6ad8a024c4556f756b36c2665ef005796bd2ee
Rafael Rodriguez <rafael.rodriguez@opendeusto.es>
2026-03-06 08:42:56 +0100

    Initial Commit
    Initial commit about unity yets

---
+++ Assets/NewMono.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NewMono : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.log("Hola mi nombre es: " + gameObject.name + "My x is" + gameObject.transform);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Variable test.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public int machine;
+private int noMachine;
+public Color colorme = Color.red;
+
+public class Variabletest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     public int machine;
+    private int noMachine;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+     public int machine;
+    private int noMachine; * 
+    }
+}
